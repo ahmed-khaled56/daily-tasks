c519cc8d60617013d448a666e8164ccb4f9b3832
ahmed <mhmamdmf275@gmail.com>
2025-05-03 19:29:39 +0300

    create color list to add bottom sheet

---
+++ lib/widgets/add_color_list.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(backgroundColor: Colors.blue, radius: 30);
+  }
+}
+
+class ColorList extends StatelessWidget {
+  const ColorList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: ColorItem(),
+          );
+        },
+      ),
+    );
+  }
+}
