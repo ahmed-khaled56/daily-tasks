eb71d368fd5119fb2f9e78de8a717af4a3d4757e
ahmed <mhmamdmf275@gmail.com>
2025-05-03 19:27:41 +0300

    create add bottom sheet

--- lib/widgets/addBottomSheetBody.dart
+++ lib/widgets/addBottomSheetBody.dart
@@ -1,3 +1,5 @@
+import 'package:daily_tasks/widgets/add_color_list.dart';
+import 'package:daily_tasks/widgets/custom_button.dart';
 import 'package:daily_tasks/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
 
@@ -6,11 +8,24 @@ class Addbottomsheetbody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        textField(hintText: 'Task Name', onchange: (value) {}),
-        textField(hintText: 'Describtion', onchange: (value) {}),
-      ],
+    return Padding(
+      padding: EdgeInsets.only(
+        top: 15,
+        left: 15,
+        right: 15,
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
+      child: Column(
+        children: [
+          textField(hintText: 'Task Name', onchange: (value) {}),
+          SizedBox(height: 40),
+          textField(hintText: 'Describtion', onchange: (value) {}),
+          SizedBox(height: 40),
+          ColorList(),
+          SizedBox(height: 40),
+          CustomButton(label: "Add", onPressed: () {}),
+        ],
+      ),
     );
   }
 }
