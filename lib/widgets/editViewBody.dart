f9b58fd2a5e60830b4b8cb6e46e82cfecd9024f6
ahmed <mhmamdmf275@gmail.com>
2025-05-03 21:13:19 +0300

    create edit view body

---
+++ lib/widgets/editViewBody.dart
@@ -0,0 +1,25 @@
+import 'package:daily_tasks/widgets/add_color_list.dart';
+import 'package:daily_tasks/widgets/edit_textField.dart';
+import 'package:flutter/material.dart';
+
+class Editviewbody extends StatelessWidget {
+  const Editviewbody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            EditTextfield(hintText: 'Task Name', onchange: (value) {}),
+            SizedBox(height: 40),
+            EditTextfield(hintText: 'Describtion', onchange: (value) {}),
+            SizedBox(height: 40),
+            ColorList(),
+            SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
+  }
+}
