62b63e8ad0853ea27b187ffe0d284ab11d6eaa36
ahmed <mhmamdmf275@gmail.com>
2025-05-06 21:27:04 +0300

    stup hive successfully

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,7 +1,16 @@
-class TaskModel {
+import 'package:hive/hive.dart';
+
+part 'task_model.g.dart';
+
+@HiveType(typeId: 0)
+class TaskModel extends HiveObject {
+  @HiveField(0)
   final String? lable;
+  @HiveField(1)
   final String? sublable;
+  @HiveField(2)
   final String? color;
+  @HiveField(3)
   final String? date;
 
   TaskModel({
